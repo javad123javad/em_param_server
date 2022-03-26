6521ecfc25d3a3c80bcd55f3336a7c10df30bcb6
Javad Rahimi <javad321javad@gmail.com>
2022-03-26 11:12:29 +0430

    UNIX ALlib added

--- src/main.cpp
+++ src/main.cpp
@@ -42,17 +42,40 @@ int32_t exec_cmd(repository & repo, std::string prompt)
     }
 
 }
+
+
+void server_cb(cpayload *payload)
+{
+
+    fprintf(stderr,"Callback: %s\n", payload->payload);
+    al_write_sock(payload->cli_sock, payload->payload, payload->payload_len);
+}
+void thr_cb(repository * repo)
+{
+    int32_t server_sock;
+    int32_t fret;
+    server_sock = al_srv_open_sock(SOCK_UNIX);
+    fret = al_unix_srv_bind(server_sock, AL_UNIX_FILE);
+    if(-1 == fret)
+    {
+        return ;
+    }
+    fret =  al_srv_listen_sock(server_sock,4);
+    if(-1 == fret)
+    {
+        perror("Listen");
+        return ;
+    }
+
+    fret = al_srv_serve_reqs(server_sock, server_cb);
+}
 int main()
 {
 
     repository repo("test_repo.txt");
     string line;
 
-    std::thread ipc_thr([](repository& repo){
-        int server_sock;
-
-
-    });
+    std::thread ipc_thr( thr_cb, &repo);
 
     repo.open_file();
     while (1) {
